f1dd83f6076838cafa13fec5e51c8d9e80227774
PRcraft <108755448+PRcraft@users.noreply.github.com>
2022-07-06 19:12:53 -0300

    Adicionar arquivos de projeto.

---
+++ exercicio 4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeros1;
+            for (numeros1 = 0; numeros1 < 51; numeros1++)
+            {
+                Console.WriteLine(numeros1);
+            }
+            Console.ReadKey();
+        }
+    }
+}
